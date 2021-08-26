f197b194209385aef9adfb773f69624575631ff4
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-27 02:54:23 +0300

    Create errors controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  to:'welcome#index'
+
+  # errors
+  get '/404', to: 'errors#not_found', as: :not_found
+  get '/422', to: 'errors#unacceptable', as: :unacceptable
+  get '/500', to: 'errors#internal_error', as: :internal_error
 end
