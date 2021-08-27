5db41f36975081c0fde986220518b34f0a57ad5e
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-27 03:28:27 +0300

    Fix devise controller error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', 
-                                    sessions: 'users/sessions_controller', 
+                                    sessions: 'users/sessions', 
                                     registrations: 'users/registration'
                                   }
 
