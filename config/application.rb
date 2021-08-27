6a47315c78189d05d8b72dbf4dffee3fba366056
nejdetkadir <nejdetkadir.550@gmail.com>
2021-08-27 07:01:55 +0300

    Change default locale to English

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module RailsBoilerplate
     config.exceptions_app = routes
 
     # I18n Config
-    config.i18n.default_locale = :tr
+    config.i18n.default_locale = :en
 
     # support Turkish and English as locales
     I18n.available_locales = %i[tr en]
